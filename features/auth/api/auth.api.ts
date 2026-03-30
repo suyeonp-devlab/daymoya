c9417f3724c6a31cb6fa7a1ab03b42148d480d2b
suyeon <suyeondevlab@gmail.com>
2026-03-31 00:19:28 +0900

    refactor: update auth API endpoints to use public path

--- features/auth/api/auth.api.ts
+++ features/auth/api/auth.api.ts
@@ -3,17 +3,17 @@ import { request, requestOrThrow } from "@/shared/api/axios";
 
 /** 로그인 */
 export const login = (data: AuthType.LoginRequest): Promise<AuthType.LoginResponse> => {
-  return requestOrThrow<AuthType.LoginResponse>({ method: "POST", url: "/auth/login", data });
+  return requestOrThrow<AuthType.LoginResponse>({ method: "POST", url: "/auth/public/login", data });
 }
 
 /** 회원가입 인증코드 전송 */
 export const sendSignupCode = async (data: AuthType.SignupCodeRequest): Promise<null> => {
-  return request<null>({ method: "POST", url: "/auth/signup/code", data })
+  return request<null>({ method: "POST", url: "/auth/public/signup/code", data })
 };
 
 /** 회원가입 인증코드 확인 */
 export const verifySignupCode = async (data: AuthType.VerifySignupCodeRequest): Promise<null> => {
-  return request<null>({ method: "POST", url: "/auth/signup/code/verify", data })
+  return request<null>({ method: "POST", url: "/auth/public/signup/code/verify", data })
 };
 
 /** 회원가입 */
@@ -25,20 +25,20 @@ export const signup = async (data: AuthType.SignupRequest): Promise<null> => {
   formData.append("passwordConfirm", data.passwordConfirm);
   formData.append("nickname", data.nickname);
 
-  return request<null>({ method: "POST", url: "/auth/signup", data: formData })
+  return request<null>({ method: "POST", url: "/auth/public/signup", data: formData })
 };
 
 /** 비밀번호 찾기 인증코드 전송 */
 export const sendPasswordForgotCode = async (data: AuthType.PasswordForgotCodeRequest): Promise<null> => {
-  return request<null>({ method: "POST", url: "/auth/password/forgot/code", data })
+  return request<null>({ method: "POST", url: "/auth/public/password/forgot/code", data })
 };
 
 /** 비밀번호 찾기 인증코드 확인 */
 export const verifyPasswordForgotCode = async (data: AuthType.VerifyPasswordForgotCodeRequest): Promise<null> => {
-  return request<null>({ method: "POST", url: "/auth/password/forgot/code/verify", data })
+  return request<null>({ method: "POST", url: "/auth/public/password/forgot/code/verify", data })
 };
 
 /** 비밀번호 변경 */
 export const resetForgottenPassword = async (data: AuthType.PasswordForgotResetRequest): Promise<null> => {
-  return request<null>({ method: "POST", url: "/auth/password/forgot/reset", data })
+  return request<null>({ method: "POST", url: "/auth/public/password/forgot/reset", data })
 };
